8c024cc21d8bf0212bfea9c694c0f8646eadc482
Soilaguela <soilabenguela@gmail.com>
2024-11-12 14:20:59 +0000

    first Deploy

---
+++ Models/PokemonOwner.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonAPI.Models
+{
+    public class PokemonOwner
+    {
+      public int Pokemonld {  get; set; }
+      public int Ownerld { get; set; }
+      public Pokemon? Pokemon { get; set; }
+      public Owner? Owner { get; set; }
+    }
+}
